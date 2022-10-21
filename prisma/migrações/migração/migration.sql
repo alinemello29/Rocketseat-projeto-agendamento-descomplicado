acac8a1c3aad925c696eff8f2e6c921f0fd927e4
Aline Melo <109696840+alinemello29@users.noreply.github.com>
2022-10-21 11:05:52 -0400

    Add files via upload

---
+++ prisma/migrações/migração/migration.sql
@@ -0,0 +1,34 @@
+-- CreateTable
+CREATE TABLE "users" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "username" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "avatar_url" TEXT,
+    "about" TEXT,
+    "created_at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+-- CreateTable
+CREATE TABLE "user_time_intervals" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "week_day" INTEGER NOT NULL,
+    "time_start_in_minutes" INTEGER NOT NULL,
+    "time_end_in_minutes" INTEGER NOT NULL,
+    "user_id" TEXT NOT NULL,
+    CONSTRAINT "user_time_intervals_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "schedulings" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "date" DATETIME NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "observations" TEXT,
+    "user_id" TEXT NOT NULL,
+    "created_at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT "schedulings_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_username_key" ON "users"("username");
